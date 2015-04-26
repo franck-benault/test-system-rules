364af7184348b9c2c33b198d252a867d09420d07
franck-benault <franck-benault@orange.fr>
2015-04-26 22:16:48 +0200

    add predefined system properties

--- src/test/java/net/franck/benault/systemrules/properties/PropertiesTestCase.java
+++ src/test/java/net/franck/benault/systemrules/properties/PropertiesTestCase.java
@@ -15,8 +15,29 @@ public class PropertiesTestCase {
 	
 
 	@Test
-	public void test() {
+	public void testExistingSystemProperties() {
+		assertNotNull("file.separator");
+		assertNotNull("java.class.path");
+		assertNotNull("java.home");
+		assertNotNull("java.vendor");
+		assertNotNull("java.vendor.url");
+		assertNotNull("java.version");
+		assertNotNull("line.separator");
+		assertNotNull("os.arch");
+		assertNotNull("os.name");
+		assertNotNull("os.version");
+		assertNotNull("path.separator");
+		assertNotNull("user.dir");
+		assertNotNull("user.home");
+		assertNotNull("user.name");
+		
+		
+	}
+	
+	@Test
+	public void testSystemGetPropertyIsEmpty() {
 		assertNull(System.getProperty("thePropertiesIsClear"));
+		
 	}
 
 }
