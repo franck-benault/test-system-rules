7e3c66c7016599ceb6bde110b5c7fd830f6c8204
franck-benault <franck-benault@orange.fr>
2015-04-26 23:20:41 +0200

    complete test getProperties()

--- src/test/java/net/franck/benault/systemrules/properties/GetPropertiesV3TestCase.java
+++ src/test/java/net/franck/benault/systemrules/properties/GetPropertiesV3TestCase.java
@@ -1,21 +1,17 @@
 package net.franck.benault.systemrules.properties;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ProvideSystemProperty;
 
 
-public class GetPropertiesV2TestCase {
+public class GetPropertiesV3TestCase {
 	
 	
 	@Rule
-	public final ProvideSystemProperty myProperty1 = new ProvideSystemProperty("theProperty1", "value1");
-	
-	@Rule
-	public final ProvideSystemProperty myProperty2 = new ProvideSystemProperty("theProperty2", null);
+	public final ProvideSystemProperty myProperty1 = new ProvideSystemProperty("theProperty1", "value1").and("theProperty2", null);
 
 	
 	@Test
