ae8e0f54cc62003f8213ed0c3f2ac23cbe435aec
franck-benault <franck-benault@orange.fr>
2015-04-26 23:31:07 +0200

    add test for system.out.println and err

---
+++ src/test/java/net/franck/benault/systemrules/properties/SystemOutErrTestCase.java
@@ -0,0 +1,33 @@
+package net.franck.benault.systemrules.properties;
+
+import static org.junit.Assert.*;
+import net.franck.benault.sample.Hello;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardErrorStreamLog;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+
+public class SystemOutErrTestCase {
+	
+	@Rule
+	public final StandardOutputStreamLog logOut = new StandardOutputStreamLog();
+
+	@Rule
+	public final StandardErrorStreamLog logError = new StandardErrorStreamLog();
+
+	
+	@Test
+	public void testOut() {
+	    Hello hello = new Hello();
+	    assertEquals("Hello", logOut.getLog());
+	}
+
+	@Test
+	public void err() {
+		Hello hello = new Hello();
+		hello.error(); 
+	    assertEquals("error...", logError.getLog());
+	}
+
+}
